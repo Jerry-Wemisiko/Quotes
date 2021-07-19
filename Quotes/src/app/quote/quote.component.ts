29ab74fe743f3d66c112b68bca62d669c691e6d6
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-07-19 13:47:59 +0300

    create an angular form

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -12,19 +12,21 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
     new Quote(1,'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.','Dr. Seuss','Jerry' ,new Date(2021,3,10)),
-    new Quote(1,'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.','Dr. Seuss','Jerry' ,new Date(2021,3,10)),
-    new Quote(1,'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.','Dr. Seuss','Jerry' ,new Date(2021,3,10)),
+    new Quote(2,'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.','Dr. Seuss','Jerry' ,new Date(2021,3,10)),
+    new Quote(3,'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.','Dr. Seuss','Jerry' ,new Date(2021,3,10)),
   ]
 
   toggleQuote(index:any){
     this.quotes[index].showQuoteDetails=!this.quotes[index].showQuoteDetails
   }
-
+  
   addQuote(quote:Quote){
     const quotesLength =this.quotes.length;
     quote.id = quotesLength +1;
     this.quotes.push(quote);
+  }
 
+  deleteQuote(index:any){
 
   }
   constructor() { }
