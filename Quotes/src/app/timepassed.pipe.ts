b222183f7212ad72c05738de6d9323d51126408e
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-07-21 09:00:20 +0300

    updated pipe typescript file

--- Quotes/src/app/timepassed.pipe.ts
+++ Quotes/src/app/timepassed.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimepassedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any): number{
     return null;
   }
 
