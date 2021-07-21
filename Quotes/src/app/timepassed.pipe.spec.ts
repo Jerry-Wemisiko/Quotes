001997e5caac95774291726d08d740356cdf6f39
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-07-21 12:42:05 +0300

    Auto-generated commit [ci skip]

--- Quotes/src/app/timepassed.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimepassedPipe } from './timepassed.pipe';
-
-describe('TimepassedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimepassedPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
