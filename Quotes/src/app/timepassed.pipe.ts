9125a181dcef8c31c175d9c28b365b86bb4e5e3a
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-07-21 09:05:14 +0300

    added a logic to our pipe

--- Quotes/src/app/timepassed.pipe.ts
+++ Quotes/src/app/timepassed.pipe.ts
@@ -6,7 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimepassedPipe implements PipeTransform {
 
   transform(value: any): number{
-    return null;
+    let today:any=new Date();
+
+    var timeDiff=Math.abs(value-today);
+    var timeSecs=timeDiff*0.001;
+    const secs=86400;
+    var passedTime=Math.floor(timeSecs/secs);
+
+    
+    return passedTime;
   }
 
 }
